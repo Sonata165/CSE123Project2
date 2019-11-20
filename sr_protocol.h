14573d63a68071f64d047159f120faf43d14ede6
Ou, Longshen <oulongshen@outlook.com>
2019-11-20 11:12:53 -0800

    small changes

--- sr_protocol.h
+++ sr_protocol.h
@@ -79,94 +79,101 @@
 /* Structure of a type11 ICMP header
  */
 struct sr_icmp_t11_hdr {
-  uint8_t icmp_type;
-  uint8_t icmp_code;
-  uint16_t icmp_sum;
-  uint32_t unused;
-  uint8_t data[ICMP_DATA_SIZE];
+    uint8_t icmp_type;
+    uint8_t icmp_code;
+    uint16_t icmp_sum;
+    uint32_t unused;
+    uint8_t data[ICMP_DATA_SIZE];
 
-} __attribute__ ((packed)) ;
-typedef struct sr_icmp_t11_hdr sr_icmp_t11_hdr_t;
+} __attribute__((packed));
+
+/* ICMP header */
+typedef struct sr_icmp_t11_hdr IcmpHeader;
 
 /*
  * Structure of an internet header, naked of options.
  */
-struct sr_ip_hdr
-  {
+struct sr_ip_hdr {
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-    unsigned int ip_hl:4;		/* header length */
-    unsigned int ip_v:4;		/* version */
+    unsigned int ip_hl : 4; /* header length */
+    unsigned int ip_v : 4; /* version */
 #elif __BYTE_ORDER == __BIG_ENDIAN
-    unsigned int ip_v:4;		/* version */
-    unsigned int ip_hl:4;		/* header length */
+    unsigned int ip_v : 4; /* version */
+    unsigned int ip_hl : 4; /* header length */
 #else
-#error "Byte ordering ot specified " 
-#endif 
-    uint8_t ip_tos;			/* type of service */
-    uint16_t ip_len;			/* total length */
-    uint16_t ip_id;			/* identification */
-    uint16_t ip_off;			/* fragment offset field */
-#define	IP_RF 0x8000			/* reserved fragment flag */
-#define	IP_DF 0x4000			/* dont fragment flag */
-#define	IP_MF 0x2000			/* more fragments flag */
-#define	IP_OFFMASK 0x1fff		/* mask for fragmenting bits */
-    uint8_t ip_ttl;			/* time to live */
-    uint8_t ip_p;			/* protocol */
-    uint16_t ip_sum;			/* checksum */
-    uint32_t ip_src, ip_dst;	/* source and dest address */
-  } __attribute__ ((packed)) ;
-typedef struct sr_ip_hdr sr_ip_hdr_t;
+#error "Byte ordering ot specified "
+#endif
+    uint8_t ip_tos; /* type of service */
+    uint16_t ip_len; /* total length */
+    uint16_t ip_id; /* identification */
+    uint16_t ip_off; /* fragment offset field */
+#define IP_RF 0x8000 /* reserved fragment flag */
+#define IP_DF 0x4000 /* dont fragment flag */
+#define IP_MF 0x2000 /* more fragments flag */
+#define IP_OFFMASK 0x1fff /* mask for fragmenting bits */
+    uint8_t ip_ttl; /* time to live */
+    uint8_t ip_p; /* protocol */
+    uint16_t ip_sum; /* checksum */
+    uint32_t ip_src, ip_dst; /* source and dest address */
+} __attribute__((packed));
+
+/* IP Header */
+typedef struct sr_ip_hdr IpHeader;
 
 /* 
- *  Ethernet packet header prototype.  Too many O/S's define this differently.
- *  Easy enough to solve that and define it here.
+ * Ethernet packet header prototype.  Too many O/S's define this differently.
+ * Easy enough to solve that and define it here.
+ * 
+ * Every IP packet sent by ethernet protocol will be added an ethernet header.
  */
-struct sr_ethernet_hdr
-{
+struct sr_ethernet_hdr {
 #ifndef ETHER_ADDR_LEN
 #define ETHER_ADDR_LEN 6
 #endif
-    uint8_t  ether_dhost[ETHER_ADDR_LEN];    /* destination ethernet address */
-    uint8_t  ether_shost[ETHER_ADDR_LEN];    /* source ethernet address */
-    uint16_t ether_type;                     /* packet type ID */
-} __attribute__ ((packed)) ;
-typedef struct sr_ethernet_hdr sr_ethernet_hdr_t;
+    uint8_t ether_dhost[ETHER_ADDR_LEN]; /* destination ethernet address */
+    uint8_t ether_shost[ETHER_ADDR_LEN]; /* source ethernet address */
+    uint16_t ether_type; /* packet type ID */
+} __attribute__((packed));
 
-
+/* Ethernet header */
+typedef struct sr_ethernet_hdr EthernetHeader;
 
 enum sr_ip_protocol {
-  ip_protocol_icmp = 0x0001,
+    ip_protocol_icmp = 0x0001,
 };
 
+/**
+ * The type of IP packet will be showed in Ethernet header.
+ * Codes below describe to different IP packet, and will be added to ethernet header.
+ */
 enum sr_ethertype {
-  ethertype_arp = 0x0806,
-  ethertype_ip = 0x0800,
+    ethertype_arp = 0x0806,
+    ethertype_ip = 0x0800,
 };
-
 
 enum sr_arp_opcode {
-  arp_op_request = 0x0001,
-  arp_op_reply = 0x0002,
+    arp_op_request = 0x0001,
+    arp_op_reply = 0x0002,
 };
 
 enum sr_arp_hrd_fmt {
-  arp_hrd_ethernet = 0x0001,
+    arp_hrd_ethernet = 0x0001,
 };
 
+struct sr_arp_hdr {
+    unsigned short ar_hrd; /* format of hardware address   */
+    unsigned short ar_pro; /* format of protocol address   */
+    unsigned char ar_hln; /* length of hardware address   */
+    unsigned char ar_pln; /* length of protocol address   */
+    unsigned short ar_op; /* ARP opcode (command)         */
+    unsigned char ar_sha[ETHER_ADDR_LEN]; /* sender hardware address      */
+    uint32_t ar_sip; /* sender IP address            */
+    unsigned char ar_tha[ETHER_ADDR_LEN]; /* target hardware address      */
+    uint32_t ar_tip; /* target IP address            */
+} __attribute__((packed));
 
-struct sr_arp_hdr
-{
-    unsigned short  ar_hrd;             /* format of hardware address   */
-    unsigned short  ar_pro;             /* format of protocol address   */
-    unsigned char   ar_hln;             /* length of hardware address   */
-    unsigned char   ar_pln;             /* length of protocol address   */
-    unsigned short  ar_op;              /* ARP opcode (command)         */
-    unsigned char   ar_sha[ETHER_ADDR_LEN];   /* sender hardware address      */
-    uint32_t        ar_sip;             /* sender IP address            */
-    unsigned char   ar_tha[ETHER_ADDR_LEN];   /* target hardware address      */
-    uint32_t        ar_tip;             /* target IP address            */
-} __attribute__ ((packed)) ;
-typedef struct sr_arp_hdr sr_arp_hdr_t;
+/* ARP header */
+typedef struct sr_arp_hdr ArpHeader;
 
 #define sr_IFACE_NAMELEN 32
 
--- sr_router.c
+++ sr_router.c
@@ -1,9 +1,4 @@
 /**********************************************************************
- * file:  sr_router.c
- * date:  Mon Feb 18 12:50:42 PST 2002
- * Contact: casado@stanford.edu
- *
- * Description:
  *
  * This file contains all the functions that interact directly
  * with the routing table, as well as the main entry method
@@ -45,17 +40,23 @@ void sr_init(struct sr_instance* sr)
     pthread_t thread;
 
     pthread_create(&thread, &(sr->attr), sr_arpcache_timeout, sr);
-    
+
     /* Add initialization code here! */
 
 } /* -- sr_init -- */
 
-/*---------------------------------------------------------------------
+/**
  * Method: sr_handlepacket(uint8_t* p,char* interface)
  * Scope:  Global
  *
+ * Parameters:
+ *   sr - a router
+ *   packet - a pointer pointing to the input Ethernet Frame
+ *   len - the length of the Frame
+ *   interface - incoming interface, where the Frame come from
+ * 
  * This method is called each time the router receives a packet on the
- * interface.  The packet buffer, the packet length and the receiving
+ * interface. The packet buffer, the packet length and the receiving
  * interface are passed in as parameters. The packet is complete with
  * ethernet headers.
  *
@@ -63,22 +64,45 @@ void sr_init(struct sr_instance* sr)
  * by sr_vns_comm.c that means do NOT delete either.  Make a copy of the
  * packet instead if you intend to keep it around beyond the scope of
  * the method call.
- *
- *---------------------------------------------------------------------*/
-
-void sr_handlepacket(struct sr_instance* sr,
-        uint8_t * packet/* lent */,
-        unsigned int len,
-        char* interface/* lent */)
+ */
+void sr_handlepacket(struct sr_instance* sr, uint8_t* packet,
+    unsigned int len, char* interface)
 {
-  /* REQUIRES */
-  assert(sr);
-  assert(packet);
-  assert(interface);
+    /* REQUIRES */
+    assert(sr);
+    assert(packet);
+    assert(interface);
 
-  printf("*** -> Received packet of length %d \n",len);
+    printf("*** -> Received packet of length %d \n", len);
 
-  /* fill in code here */
+    /* fill in code here */
+    print_hdrs(packet, len);
+    EthernetHeader* eth_hdr = (EthernetHeader*)packet;
+    uint16_t type = ntohs(eth_hdr->ether_type);
+    if (type == ethertype_arp) {
+        printf("I'm from ARP!\n");
+        handle_arp_packet(sr, packet, len, interface);
+    }
+    else if (type == ethertype_ip) {
+        printf("I'm from IP!\n");
+        handle_ip_packet(sr, packet, len, interface);
+    }
+    else {
+        fprintf(stderr, "Invalid packet type: %x\n", type);
+    }
+}
 
-}/* end sr_ForwardPacket */
+void handle_arp_packet(struct sr_instance* sr, uint8_t* packet,
+    unsigned int len, char* interface) {
+    /* Check length */
+    int min_length = sizeof(EthernetHeader) + sizeof(ArpHeader);
+    if (len < min_length) {
+        fprintf("Invalid ARP length: %d\n", len);
+        continue;
+    }
+}
 
+void handle_ip_packet(struct sr_instance* sr, uint8_t* packet,
+    unsigned int len, char* interface) {
+
+}
